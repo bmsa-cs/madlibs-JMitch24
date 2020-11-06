10b363eb86478bf3be2ccf3a1fa2ea5c1c6c7e6a
Jesse Mitchell <jmitchell24@biomedscienceacademy.org>
2020-11-06 22:13:05 +0000

    Initial starting place of code and trying some things out.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Jesse Mitchell
+Period/Core: Core 6
 
 
 """
+
+print("Let's Play Silly Sentences! \n")
+name_input = input("Enter a name: ")
+second_name_input = input("Enter another diffrent name: ")
+city_input = input("Enter the name of a city: ")
