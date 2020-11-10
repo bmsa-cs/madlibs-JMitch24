a7359cd901c3f179fd2f933694a6938af9358a4b
Jesse Mitchell <jmitchell24@biomedscienceacademy.org>
2020-11-10 12:25:53 +0000

    First set of user inputs

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ print("Let's Play Silly Sentences! \n")
 name_input = input("Enter a name: ")
 second_name_input = input("Enter another diffrent name: ")
 city_input = input("Enter the name of a city: ")
+size_adjective = input("Give a size adjective: ")
+type_of_pet = input("Give a animal: ")
+time_span = int(input("Give an age (writen as a number): "))
+
+
